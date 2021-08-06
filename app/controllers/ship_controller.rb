96738ae088d6e3b4da51a3366ebb53a80104a257
mkpdev <mukpdev11@gmail.com>
2021-08-06 10:46:28 +0530

    Add faker gem

--- app/controllers/ship_controller.rb
+++ app/controllers/ship_controller.rb
@@ -1,8 +1,13 @@
 class ShipController < ApplicationController
 
+  require 'faker'
+
   def new
-  	render json: { ship_params:{
-		name: "Abc"
-	}};
+    sleep(rand(30..90))
+    render json: { ship_params:{
+      name: Faker::Name.unique.name,
+      origin: Faker::Company.name,
+      passengers: Faker::Number.between(from: 1, to: 10)
+    }};
   end
 end
